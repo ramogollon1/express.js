c0faa007d9a611ace281ab4bc7247826e616b119
rafael <rafaelerasmo6@gmail.com>
2017-09-17 23:14:08 -0600

    add express with html

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-// require express module
 var express = require('express')
-// ejecutando express
 var app = express()
-// require http module local
 var http = require('http').Server(app)
-// definiendo puerto
+
 var port = 8080
-// call index '/'
+
 app.get('/', function(req, res) {
-	res.send('<h1>Hello World</h1>')
+	res.send(__dirname + '/index.html')
 })
-// escuchar puerto 8080
+
 http.listen(port, function(err){
 	if (err) return console.log(err)
 	console.log('servidor escuchando en: ', 8080)
